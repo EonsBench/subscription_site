2ba0270605821437525fd7e3078cfc54bfc2405b
EonsBench <wadengui@gmail.com>
2024-07-10 23:53:26 +0900

    사용자 라우트 추가

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const passport = require('./config/passport')
+const userRoute = require('./routes/user');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
 
 connectDB();
+
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)

4f6bf53c992ca8cf0e404405d4bee46977288c49
EonsBench <wadengui@gmail.com>
2024-07-12 00:01:22 +0900

    feat: 회원가입 및 로그인 시 유효성 체크 기능 추가
    - 회원가입 시 이메일, 이름, 패스워드를 검사하고 패스워드에 제약을 추가
    - 로그인 시 이메일이 유효한지 비밀번호를 입력했는지 검사

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,19 +3,39 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const User = require('../models/users');
+const {body, validationResult} = require('express-validator');
 const router = express.Router();
 
-router.post('/register', async (req, res)=>{
-    const {email, username, password, isContentCreator} = req.body;
-    try{
-        const user = new User({email, username, password, isContentCreator});
-        await user.save();
-        res.status(201).send('가입이 완료되었습니다.');
-    }catch(error){
-        res.status(400).send('가입에 실패하였습니다');
-    }
+router.post('/register', [
+    body('email').isEmail().withMessage('유효하지 않은 이메일입니다.'),
+    body('username').notEmpty().withMessage('사용자 이름을 입력해주세요.'),
+    body('password')
+    .isLength({min:8}).withMessage('비밀번호는 최소 8자 이상이어야 합니다.')
+    .matches(/[A-Z]/).withMessage('최소 하나의 대문자가 포함되어야 합니다.')
+    .matches(/[0-9]/).withMessage('최소 하나의 숫자가 포함되어야 합니다.')
+    .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('최소 하나의 특수문자가 포함되어야 합니다.'),
+    ], async (req, res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        const {email, username, password, isContentCreator} = req.body;
+        try{
+            const user = new User({email, username, password, isContentCreator});
+            await user.save();
+            res.status(201).send('가입이 완료되었습니다.');
+        }catch(error){
+            res.status(400).send('가입에 실패하였습니다');
+        }
 });
-router.post('/login', async (req, res, next)=>{
+router.post('/login', [
+    body('email').isEmail().withMessage('유효하지 않은 이메일입니다.'),
+    body('password').notEmpty().withMessage('비밀번호를 입력해주세요.')
+], async (req, res, next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
     passport.authenticate('local', {session: false}, (err, user, info) =>{
         if(err){
             return next(err);

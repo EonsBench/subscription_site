5c9ea3395ffe40c331ed041109d5b3e2be92c0a5
EonsBench <wadengui@gmail.com>
2024-07-15 22:39:55 +0900

    id 필드 이름 변경

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ContentSchema = new mongoose.Schema({
-    creator:{type: mongoose.Schema.ObjectId, ref: 'Creator', required: true},
+    creator_id:{type: mongoose.Schema.ObjectId, ref: 'Creator', required: true},
     title:{type:String, required: true},
     content: {type: String, required: true},
     createAt:{type:Date, default: Date.now},
